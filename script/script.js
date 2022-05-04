0f4dbccabc104c29cc09986aeac43ad4a0244eaa
GiuliaV <valente.giulia93@gmail.com>
2022-05-04 16:40:47 +0200

    Script  corretto

--- script/script.js
+++ script/script.js
@@ -1,16 +1,15 @@
 //1. Programma chiede all'utente i Km e l'età
 
-var percorso = parseInt(prompt("Quanti chilometri vuoi percorrere?"));
-console.log(percorso);
-var anni = parseInt (prompt("Quanti anni hai?"));
-console.log(anni);
+const percorso = parseInt(prompt("Quanti chilometri vuoi percorrere?"));
+console.log("Km: " + percorso);
+const anni = parseInt (prompt("Quanti anni hai?"));
+console.log("Età: " + anni);
 //2. Su queste info calcola il prezzo del biglietto
-var prezzoKm = 0.21;
-var prezzoIntero = prezzoKm * percorso;
-var text = "Il prezzo del biglietto intero é:";
-var textSconto = "Il prezzo del biglietto scontato è:"
-var euro = "euro";
-console.log (prezzoIntero);
+const prezzoKm = 0.21;
+const prezzoIntero = prezzoKm * percorso;
+const text = "Il prezzo del biglietto intero é: ";
+const textSconto = "Il prezzo del biglietto scontato è: "
+const euro = " euro";
 
 //3. Se età >= 18  sconto del 20%
 //   Se età <=65 sconto del 40%
@@ -18,12 +17,17 @@ console.log (prezzoIntero);
 
 if (anni <= 18) {
     document.getElementById('costo').innerHTML = text + (prezzoIntero.toFixed(2)) + euro;
-    document.getElementById('sconto').innerHTML = textSconto + (((prezzoIntero / 100) * 20).toFixed(2)) + euro;
+    console.log (text + prezzoIntero + euro);
+    document.getElementById('sconto').innerHTML = textSconto + (((prezzoIntero / 100) * 80).toFixed(2)) + euro;
+    console.log (textSconto + ((prezzoIntero / 100) * 80).toFixed(2) + euro);
 }
 else if (anni >= 65) {
     document.getElementById('costo').innerHTML = text + (prezzoIntero) + euro;
-    document.getElementById('sconto').innerHTML = textSconto + (((prezzoIntero / 100) * 40).toFixed(2)) + euro;
+    console.log (text + prezzoIntero);
+    document.getElementById('sconto').innerHTML = textSconto + (((prezzoIntero / 100) * 60).toFixed(2)) + euro;
+    console.log (textSconto + ((prezzoIntero / 100) * 60).toFixed(2) + euro);
 }
 else {
     document.getElementById('costo').innerHTML = text + (prezzoIntero.toFixed(2)) + euro;
+    console.log (text + prezzoIntero + euro);
 }
