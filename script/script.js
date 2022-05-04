383e34b77de1247957c937f71aff0dd8c42cef28
GiuliaV <valente.giulia93@gmail.com>
2022-05-04 16:59:13 +0200

    Script - modifica messaggio a video

--- script/script.js
+++ script/script.js
@@ -7,27 +7,27 @@ console.log("Età: " + anni);
 //2. Su queste info calcola il prezzo del biglietto
 const prezzoKm = 0.21;
 const prezzoIntero = prezzoKm * percorso;
-const text = "Il prezzo del biglietto intero é: ";
-const textSconto = "Il prezzo del biglietto scontato è: "
-const euro = " euro";
 
-//3. Se età >= 18  sconto del 20%
-//   Se età <=65 sconto del 40%
+    //Testo da stampare a vieo
+    const text = "Il prezzo del biglietto intero é: ";
+    const textSconto = "Il prezzo del biglietto scontato è: "
+    const euro = " euro";
+
+//3. Se età <= 18 sconto del 20%
+//   Se età >= 65 sconto del 40%
 //4. Stampare a video il prezzo con e senza sconto
 
+document.getElementById('costo').innerHTML = text + (prezzoIntero.toFixed(2)) + euro;
+console.log (text + prezzoIntero + euro);
+
+
 if (anni <= 18) {
-    document.getElementById('costo').innerHTML = text + (prezzoIntero.toFixed(2)) + euro;
-    console.log (text + prezzoIntero + euro);
+
     document.getElementById('sconto').innerHTML = textSconto + (((prezzoIntero / 100) * 80).toFixed(2)) + euro;
     console.log (textSconto + ((prezzoIntero / 100) * 80).toFixed(2) + euro);
 }
 else if (anni >= 65) {
-    document.getElementById('costo').innerHTML = text + (prezzoIntero) + euro;
-    console.log (text + prezzoIntero);
+
     document.getElementById('sconto').innerHTML = textSconto + (((prezzoIntero / 100) * 60).toFixed(2)) + euro;
     console.log (textSconto + ((prezzoIntero / 100) * 60).toFixed(2) + euro);
-}
-else {
-    document.getElementById('costo').innerHTML = text + (prezzoIntero.toFixed(2)) + euro;
-    console.log (text + prezzoIntero + euro);
 }
